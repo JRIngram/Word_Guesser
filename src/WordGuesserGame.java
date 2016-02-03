336e7bed13939b012c0546f70a58a9ab377d88f3
Jamie Ingram <jr-ingram@hotmail.co.uk>
2016-02-03 20:20:17 +0000

    Further refactoring of guessChecker method.

--- src/WordGuesserGame.java
+++ src/WordGuesserGame.java
@@ -11,26 +11,9 @@ public class WordGuesserGame{
 	public static void main(String[] args) {
 				
 		wordGen = new WordGenerator();
-		//words = new String[10]; No longer needed, kept for testing
 		input = new Scanner(System.in);
 		boolean correct = false;
-		
-		//Generates 10 words.
-		/*pre-refactoring
-		 * 
-		 * System.out.print("Welcome to WordGuesser!\nHow many letters would you like in the words? Must be between 5-10.\n");	
-		 * System.out.print("> ");
-		 * 
-		 * for(int i = 0; i < 10; i++){
-			words[i] = wordGen.generateWord(wordLength);
-			//If the new word is the same as a previous word, regenerate.
-			for(int c = i - 1; c >= 0 ; c--){
-				while(words[i].equals(words[c])){
-					words[i] = wordGen.generateWord(wordLength);
-				}
-			}
-		}*/
-		
+			
 		System.out.print("Welcome to WordGuesser!\nHow many letters would you like in the words? Must be between 5-10.\n");
 		System.out.print("> ");
 		wordGen.makeWordList(input.nextInt(), 5, 10);
@@ -60,20 +43,10 @@ public class WordGuesserGame{
 		System.out.println("Begin by choosing the number of the word you want to guess, or enter 0 or a negative number to exit.:");
 	}
 	
-	//Randomly chooses a word out of the words Array.
-	/*
-	 * pre-refactoring 
-	 * ---METHOD---
-	 * public static void wordChooser(){
-		Random wordChooser = new Random();
-		int wordNumber = wordChooser.nextInt(10);
-		chosenWord = words[wordNumber]; 
-	}*/
-		
 	//Used to check number of guesses.
-	public static boolean maxGuesses(){
-		if(numberOfGuesses >= 4){
-			System.out.println("You have had your 4 guesses and haven't guessed the correct word!\nYou lose!\nThe correct word was: " + chosenWord + ".");
+	public static boolean maxGuesses(int maxGuesses){
+		if(numberOfGuesses >= maxGuesses){
+			System.out.println("You have had your" + maxGuesses + "guesses and haven't guessed the correct word!\nYou lose!\nThe correct word was: " + chosenWord + ".");
 			return true;
 		}
 		else if(numberOfGuesses > 1){
@@ -109,18 +82,10 @@ public class WordGuesserGame{
 			return true;		
 		}
 		
-		//Splits string into characters and compares each character	
+		//Displays similarity between guessed word and chosen word.
 		else{
-			int similarity = 0;
-			String chosenWordLetters[] = chosenWord.split("");
-			String guessLetters[] = wordGen.getWord(guess).split("");
-			for(int i = 0; i < chosenWordLetters.length; i++ ){
-				if(guessLetters[i].equals(chosenWordLetters[i])){
-					similarity++;
-				}
-			}
-			System.out.println("You chose " + wordGen.getWord(guess) + ". This has a similarity of " + similarity + "/" + wordGen.getWordLength() + ".");
-			if(!maxGuesses()){
+			System.out.println("You chose " + wordGen.getWord(guess) + ". This has a similarity of " + getSimilarity(guess) + "/" + wordGen.getWordLength() + ".");
+			if(!maxGuesses(4)){
 				System.out.print("Guess another word, or enter 0 or a negative number to exit.\n");
 				return false;
 			} 
@@ -128,6 +93,22 @@ public class WordGuesserGame{
 				return true;
 			}
 		}
+	}
+	
+	/*Calculates similarity of guessed word and actual word.
+	 * Splits string into characters and compares each character.
+	 */
+	
+	public static int getSimilarity(int guess){
+		int similarity = 0;
+		String chosenWordLetters[] = chosenWord.split("");
+		String guessLetters[] = wordGen.getWord(guess).split("");
+		for(int i = 0; i < chosenWordLetters.length; i++ ){
+			if(guessLetters[i].equals(chosenWordLetters[i])){
+				similarity++;
+			}
+		}
+		return similarity;
 	}
 }
 
